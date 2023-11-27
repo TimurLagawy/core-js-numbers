fbb02cc780bb383a4554ffba467abf86223eee27
Timur Lagawy <timurtagirovich@mail.ru>
2023-11-27 18:22:42 +0100

    feat: 6 tasks passed

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -84,8 +84,8 @@ function getDistanceBetweenPoints(/* x1, y1, x2, y2 */) {
  *   x + 8 = 0       => -8
  *   5*x = 0         => 0
  */
-function getLinearEquationRoot(/* a, b */) {
-  throw new Error('Not implemented');
+function getLinearEquationRoot(a, b) {
+  return -b / a;
 }
 
 /**
@@ -122,8 +122,14 @@ function getAngleBetweenVectors(/* x1, y1, x2, y2 */) {
  *     5     => 5
  *     0     => 0
  */
-function getLastDigit(/* value */) {
-  throw new Error('Not implemented');
+function getLastDigit(value) {
+  let res;
+  if (typeof value === 'number') {
+    res = value % 10;
+  } else {
+    res = false;
+  }
+  return res;
 }
 
 /**
