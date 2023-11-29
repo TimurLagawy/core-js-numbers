0dcf73e48409920f1d21b224d16c7cad22580aec
Timur Lagawy <timurtagirovich@mail.ru>
2023-11-29 13:16:40 +0100

    feat: add 24 completed tests

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -68,8 +68,8 @@ function getAverage(value1, value2) {
  *   (0,0) (1,0)    => 1
  *   (-5,0) (10,-10) => 18.027756377319946
  */
-function getDistanceBetweenPoints(/* x1, y1, x2, y2 */) {
-  throw new Error('Not implemented');
+function getDistanceBetweenPoints(x1, y1, x2, y2) {
+  return Math.sqrt((x1 - x2) ** 2 + (y1 - y2) ** 2);
 }
 
 /**
@@ -206,8 +206,12 @@ function roundToPowerOfTen(num, pow) {
  */
 function isPrime(n) {
   let res = true;
-  if (n < 4 && (n % 2 === 0 || n % 3 === 0 || n % 5 === 0 || n % 7 === 0)) {
-    res = false;
+  if (n > 3) {
+    for (let i = 2; i < n - 1; i += 1) {
+      if (n % i === 0) {
+        res = false;
+      }
+    }
   }
   return res;
 }
